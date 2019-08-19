afaff3da91749fc44008405f2b0edd1cd33408a6
Katie Wilday <wildayk@gmail.com>
2019-08-18 21:13:58 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-50.times do
+200.times do
   Restaurant.create(r_name: Faker::Restaurant.name, r_type: Faker::Restaurant.type,
     r_description: Faker::Restaurant.description , r_review: Faker::Restaurant.review,
     rating: Faker::Number.between(from: 1, to: 5))
