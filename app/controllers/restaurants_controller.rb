1e3bef93d377e5677218d112ba2a931be2937c65
Katie Wilday <wildayk@gmail.com>
2019-09-01 21:04:13 -0400

    added like column

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,9 +15,14 @@ class RestaurantsController < ApplicationController
     render json: @restaurant
   end
 
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    render json: @restaurant
+  end
   private
 
     def restaurant_params
-      params.require(:restaurant).permit(:r_name, :r_type, :r_description, :r_review, :rating)
+      params.require(:restaurant).permit(:r_name, :r_type, :r_description, :r_review, :rating, :likes)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_205059) do
+ActiveRecord::Schema.define(version: 2019_09_01_211112) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "r_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_205059) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rating"
+    t.integer "likes"
   end
 
 end
